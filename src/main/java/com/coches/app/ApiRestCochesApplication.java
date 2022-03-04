ddc27f24e409e2a50df2ae6a66575c501bebef18
psariego <pablo.sariego-alonso@capgemini.com>
2022-03-04 15:44:23 +0100

    Creación del proyecto

---
+++ src/main/java/com/coches/app/ApiRestCochesApplication.java
@@ -0,0 +1,13 @@
+package com.coches.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCochesApplication.class, args);
+	}
+
+}
